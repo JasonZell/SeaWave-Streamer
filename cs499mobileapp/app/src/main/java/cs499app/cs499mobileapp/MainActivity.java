688aaf69b65ff099ba4c63f9ea8b6d1f0fc909c6
JasonZell <centarox@hotmail.com>
2017-06-21 12:19:47 -0700

    add music service and set up in app broadcast.

--- cs499mobileapp/app/src/main/java/cs499app/cs499mobileapp/MainActivity.java
+++ cs499mobileapp/app/src/main/java/cs499app/cs499mobileapp/MainActivity.java
@@ -1,10 +1,12 @@
 package cs499app.cs499mobileapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Start Music Service
+        Intent startServiceIntent = new Intent(MainActivity.this, MusicService.class);
+        startServiceIntent.setAction("MUSIC_ACTION_CREATE");
+        startService(startServiceIntent);
+
+        Log.d("PASSED ","passed");
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
+
     }
 
     @Override
@@ -49,4 +62,64 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    //Method: onPause
+    //Purpose: calls when the activity is paused
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.e("MENU ACTIVITY","ACTIVITY PAUSED");
+        sendBroadcast(new Intent(getString(R.string.MUSIC_ACTION_PAUSE)), getString(R.string.BROADCAST_PIVATE));
+
+//           Log.e("MENU ACTIVITY", "TRANSITION: " + isTransition);
+//        if(!isTransition) {
+//            sendBroadcast(new Intent(getString(R.string.MUSIC_ACTION_PAUSE)), getString(R.string.BROADCAST_PIVATE));
+//        }
+    }
+    //method: onResume
+    //Purpose: override method, called when activity goes into resumed state
+    //calls music service to restart the music.
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        //updateMusicIcon();
+        Log.e("MENU ACTIVITY","ACTIVITY RESUMED");
+
+        sendBroadcast(new Intent(getString(R.string.MUSIC_ACTION_PLAY)), getString(R.string.BROADCAST_PIVATE));
+        //isTransition = false;
+
+    }
+
+    //method: onStop
+    //Purpose: override method, called when activity goes into stopped state
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.e("MENU ACTIVITY","ACTIVITY STOPPED");
+    }
+
+    //method: onDestroy
+    //purpose: override method, called when activity is destroyed
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.e("MENU ACTIVITY","ACTIVITY DESTROYED");
+
+    }
+
+    //Method: onStart
+    //Purpose: override method, called when activity is started
+    //send signal to music service to start playing music
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //isTransition = false; //very important when transitioning back via back button
+        Log.e("MENU ACTIVITY", "ACTIVITY STARTED");
+    }
+
 }
---
+++ cs499mobileapp/app/src/main/java/cs499app/cs499mobileapp/MusicService.java
@@ -0,0 +1,219 @@
+package cs499app.cs499mobileapp;
+
+/**
+ * Created by centa on 6/21/2017.
+ */
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+
+
+public class MusicService extends Service implements MediaPlayer.OnErrorListener, MediaPlayer.OnPreparedListener{
+
+
+    enum myPlayerState {
+        Stopped, //MediaPlayer stopped
+        Prepared, //MediaPlayer is prepared
+        Playing, //MediaPlayer is playing
+        Paused, //MediaPlayer is paused
+        PlayerPaused,
+    }
+
+    MediaPlayer myPlayer = null;
+    myPlayerState playerState;
+
+    private BroadcastReceiver musicToggleReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().equals(getString(R.string.MUSIC_PLAY_PAUSE_TOGGLE)))
+            {
+                Log.e("MUSIC RECEIVER IN MUSIC", "MUSIC TOGGLED");
+                if(myPlayer.isPlaying())
+                {
+                    playerState = myPlayerState.PlayerPaused;
+                    myPlayer.pause();
+                }
+                else
+                {
+                    myPlayer.start();
+                    playerState = myPlayerState.Playing;
+                }
+
+            }
+        }
+    };
+
+    private BroadcastReceiver musicPlayReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().equals(getString(R.string.MUSIC_ACTION_PLAY)))
+            {
+                Log.e("MUSIC RECEIVER IN MUSIC", "MUSIC PLAYED");
+                if(playerState == myPlayerState.Paused) {
+
+                    myPlayer.start();
+                    playerState = myPlayerState.Playing;
+                }
+                else if(playerState == myPlayerState.Stopped)
+                {
+                    myPlayer.start();
+                    playerState = myPlayerState.Playing;
+                }
+
+            }
+        }
+    };
+
+    private BroadcastReceiver musicStopReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().equals(getString(R.string.MUSIC_ACTION_STOP)))
+            {
+
+                if(playerState == myPlayerState.Playing) {
+                    Log.e("MUSIC RECEIVER IN MUSIC", "MUSIC STOPPED");
+                    myPlayer.stop();
+                    playerState = myPlayerState.Stopped;
+                }
+
+            }
+        }
+    };
+
+    private BroadcastReceiver musicPauseReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.e("INSIDE PAUSED RECEIVER","INSIDE PAUSED REVEICER");
+            if(intent.getAction().equals(getString(R.string.MUSIC_ACTION_PAUSE)))
+            {
+                Log.e("MUSIC RECEIVER IN MUSIC", "MUSIC PAUSED");
+                if(playerState == myPlayerState.Playing) {
+
+                    myPlayer.pause();
+                    playerState = myPlayerState.Paused;
+                }
+
+            }
+        }
+    };
+
+    //Method: onStartCommand
+    //Purpose: method is called when the service is started
+    //setup all the receivers that the service is associated with
+    //
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        //Music receiver
+        registerReceiver(musicToggleReceiver,new IntentFilter(getString(R.string.MUSIC_PLAY_PAUSE_TOGGLE)),getString(R.string.BROADCAST_PIVATE),null);
+        registerReceiver(musicPlayReceiver,new IntentFilter(getString(R.string.MUSIC_ACTION_PLAY)),getString(R.string.BROADCAST_PIVATE),null);
+        registerReceiver(musicStopReceiver,new IntentFilter(getString(R.string.MUSIC_ACTION_STOP)),getString(R.string.BROADCAST_PIVATE),null);
+        registerReceiver(musicPauseReceiver,new IntentFilter(getString(R.string.MUSIC_ACTION_PAUSE)),getString(R.string.BROADCAST_PIVATE),null);
+
+        Log.d("INSISDE MUSIC SERVICE","MUSIC SERVICE");
+        if (intent.getAction().equals(getString(R.string.MUSIC_ACTION_CREATE))) {
+            Log.e("MUSIC","MUSIC_ACTION_CREATE FIRED");
+
+            if(myPlayer == null) {
+                {
+                    // myPlayer = MediaPlayer.create(this, R.raw.lullaby);
+                    //AssetFileDescriptor musicAsset = getResources().openRawResourceFd(R.raw.lullaby);
+                    String url = "http://174.36.206.197:8000";
+                    String url2= "http://s3.voscast.com:8456";
+                    myPlayer = new MediaPlayer();
+                    try {
+                        //myPlayer.setDataSource(musicAsset.getFileDescriptor(),musicAsset.getStartOffset(),musicAsset.getLength());
+                       // myPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                        myPlayer.setAudioAttributes(new AudioAttributes.Builder()
+                                .setUsage(AudioAttributes.USAGE_MEDIA)
+                                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                                .build());
+                        myPlayer.setDataSource(getEncodedURL(url));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    myPlayer.setOnPreparedListener(this);
+                    myPlayer.setOnErrorListener(this);
+
+
+                    // myPlayer.prepareAsync();
+
+                    try {
+                        myPlayer.prepare(); // see onPrepared()
+
+                    } catch (IOException e) {
+                        Log.e("MUSIC","PREPARATION FAILED");
+                        e.printStackTrace();
+                    }
+
+                    Log.e("MUSIC","created and prepared player");
+                }
+            }
+        }
+        return START_NOT_STICKY; // service started but will not restart when it is destroyed
+    }
+
+    public String getEncodedURL(String urlStr) throws Exception
+    {
+        URL url = new URL(urlStr);
+        URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
+        urlStr = uri.toASCIIString();
+        return urlStr;
+    }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        Log.e("MUSIC SERVICE:", "onERROR Triggered within service");
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        //myPlayer.setLooping(true);
+        Log.e("MUSIC SERVICE:", "PREPARATION FINISHED,PLAYING");
+        playerState = myPlayerState.Playing;
+        // myPlayer.start();
+        mp.start();
+    }
+
+
+    //Method: onDestroy
+    //Purpose: unregister receiver and release music player resource
+    @Override
+    public void onDestroy()
+    {
+        if(myPlayer != null)
+        {
+            myPlayer.release();
+        }
+        myPlayer = null;
+
+        //destroy music broadcast receiver
+        unregisterReceiver(musicToggleReceiver);
+        unregisterReceiver(musicPauseReceiver);
+        unregisterReceiver(musicStopReceiver);
+        unregisterReceiver(musicPlayReceiver);
+    }
+}
