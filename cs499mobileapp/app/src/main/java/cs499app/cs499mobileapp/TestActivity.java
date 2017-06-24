c8663c71068f836ece9a0c9ff2fc7f8887e45549
JasonZell <centarox@hotmail.com>
2017-06-24 00:21:12 -0700

    modify circular progress bar

--- cs499mobileapp/app/src/main/java/cs499app/cs499mobileapp/TestActivity.java
+++ cs499mobileapp/app/src/main/java/cs499app/cs499mobileapp/TestActivity.java
@@ -3,6 +3,8 @@ package cs499app.cs499mobileapp;
 import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ProgressBar;
 
@@ -13,9 +15,9 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        ObjectAnimator animation = ObjectAnimator.ofInt (progressBar, "progress", 0, 500); // see this max value coming back here, we animale towards that value
-        animation.setDuration (5000); //in milliseconds
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        ObjectAnimator animation = ObjectAnimator.ofInt (progressBar, "progress", 0, 100); // see this max value coming back here, we animale towards that value
+        animation.setDuration (10000); //in milliseconds
         animation.setInterpolator (new DecelerateInterpolator());
         animation.start ();
     }
