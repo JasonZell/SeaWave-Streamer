5a813652fc87a844a82d394edf1c92b6ef8f9e91
JasonZell <centarox@hotmail.com>
2017-06-26 16:04:29 -0700

    try different lib for arc progression

--- cs499mobileapp/app/src/main/java/cs499app/cs499mobileapp/TestActivity.java
+++ cs499mobileapp/app/src/main/java/cs499app/cs499mobileapp/TestActivity.java
@@ -3,10 +3,10 @@ package cs499app.cs499mobileapp;
 import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ProgressBar;
+
+import com.github.lzyzsd.circleprogress.ArcProgress;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -15,10 +15,14 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        ObjectAnimator animation = ObjectAnimator.ofInt (progressBar, "progress", 0, 100); // see this max value coming back here, we animale towards that value
-        animation.setDuration (10000); //in milliseconds
-        animation.setInterpolator (new DecelerateInterpolator());
-        animation.start ();
+        ArcProgress arcProgress = (ArcProgress)findViewById(R.id.arc_progress);
+        arcProgress.setBottomText("Test");
+        arcProgress.setProgress(70);
+
+//        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+//        ObjectAnimator animation = ObjectAnimator.ofInt (progressBar, "progress", 0, 100); // see this max value coming back here, we animale towards that value
+//        animation.setDuration (10000); //in milliseconds
+//        animation.setInterpolator (new DecelerateInterpolator());
+//        animation.start ();
     }
 }
